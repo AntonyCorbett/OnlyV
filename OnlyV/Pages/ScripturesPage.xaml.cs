5a4b035bbdfd7ee600969caffa817110dc5d0fae
AntonyCorbett <antony@corbetts.org.uk>
2018-09-18 05:10:41 +0100

    bring buttons into view on navigation to settings page

--- OnlyV/Pages/ScripturesPage.xaml.cs
+++ OnlyV/Pages/ScripturesPage.xaml.cs
@@ -1,6 +1,8 @@
 ﻿namespace OnlyV.Pages
 {
+    using System.Windows;
     using System.Windows.Controls;
+    using ViewModel;
 
     /// <summary>
     /// Interaction logic for ScripturesPage.xaml
@@ -10,6 +12,53 @@
         public ScripturesPage()
         {
             InitializeComponent();
+        }
+
+        private void UserControl_Loaded(object sender, System.Windows.RoutedEventArgs e)
+        {
+            if (!BringButtonIntoView(HebrewBooksControl))
+            {
+                BringButtonIntoView(GreekBooksControl);
+            }
+
+            BringButtonIntoView(ChaptersControl);
+            BringVerseButtonIntoView(VersesControl);
+        }
+
+        private bool BringButtonIntoView(ItemsControl itemsControl)
+        {
+            foreach (var item in itemsControl.Items)
+            {
+                if (item is ButtonModel bm)
+                {
+                    if (bm.Selected)
+                    {
+                        var b = itemsControl.ItemContainerGenerator.ContainerFromItem(item) as FrameworkElement;
+                        b?.BringIntoView();
+                        return true;
+                    }
+                }
+            }
+
+            return false;
+        }
+
+        private bool BringVerseButtonIntoView(ItemsControl itemsControl)
+        {
+            foreach (var item in itemsControl.Items)
+            {
+                if (item is VerseButtonModel bm)
+                {
+                    if (bm.Selected)
+                    {
+                        var b = itemsControl.ItemContainerGenerator.ContainerFromItem(item) as FrameworkElement;
+                        b?.BringIntoView();
+                        return true;
+                    }
+                }
+            }
+
+            return false;
         }
     }
 }
