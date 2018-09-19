e50f5c39e2daaf8883270ed6a7a0cf249c77930e
AntonyCorbett <antony@corbetts.org.uk>
2018-09-19 23:36:20 +0100

    app mutex

--- OnlyVThemeCreator/App.xaml.cs
+++ OnlyVThemeCreator/App.xaml.cs
@@ -1,16 +1,60 @@
-﻿namespace OnlyVThemeCreator
+﻿using System.IO;
+using OnlyVThemeCreator.Helpers;
+
+namespace OnlyVThemeCreator
 {
+    using System.Threading;
     using System.Windows;
     using GalaSoft.MvvmLight.Threading;
+    using Serilog;
 
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
     public partial class App : Application
     {
+        private readonly string _appString = "OnlyVThemeCreator";
+        private Mutex _appMutex;
+
         public App()
         {
             DispatcherHelper.Initialize();
+        }
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            _appMutex?.Dispose();
+            Log.Logger.Information("==== Exit ====");
+        }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            if (AnotherInstanceRunning())
+            {
+                Shutdown();
+            }
+            else
+            {
+                ConfigureLogger();
+            }
+        }
+
+        private void ConfigureLogger()
+        {
+            string logsDirectory = FileUtils.GetLogFolder();
+
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Information()
+                .WriteTo.RollingFile(Path.Combine(logsDirectory, "log-{Date}.txt"), retainedFileCountLimit: 28)
+                .CreateLogger();
+
+            Log.Logger.Information("==== Launched ====");
+        }
+
+        private bool AnotherInstanceRunning()
+        {
+            _appMutex = new Mutex(true, _appString, out var newInstance);
+            return !newInstance;
         }
     }
 }
