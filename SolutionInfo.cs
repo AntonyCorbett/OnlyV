43efeadb5dbbc6f92ddd6c193841aac367053889
AntonyCorbett <antony@corbetts.org.uk>
2019-12-26 12:54:02 +0000

    bump ver

--- SolutionInfo.cs
+++ SolutionInfo.cs
@@ -3,8 +3,8 @@
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Antony Corbett")]
 [assembly: AssemblyProduct("OnlyV Bible Image Text Creator")]
-[assembly: AssemblyCopyright("Copyright © 2018, 2019 Antony Corbett")]
+[assembly: AssemblyCopyright("Copyright © 2018, 2020 Antony Corbett")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("1.2.0.29")]
+[assembly: AssemblyVersion("1.2.0.30")]
